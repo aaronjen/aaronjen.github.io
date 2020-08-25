398f34d079c96ac6d3acbe452e1adc23f4321199
Aaron Jen <aaronjen@jubo.health>
2020-08-25 19:55:59 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 hugo
 
+cp -rf ./public ./temp
+cp -rf ./.git ./temp
+cd ./temp
+git branch -D master
 git checkout --orphan master
 git rm -rf .
-git add ./public
+git add .
 git commit -m "Update"
+git push -f origin master
+cd ..
+rm -rf ./temp
